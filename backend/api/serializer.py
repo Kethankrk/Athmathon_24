8d8ef9bbaf5542f089ae50814373dc72d0aace47
Kethankrk <kethankrk@gmail.com>
2024-05-02 14:32:42 +0530

    modified user auth serializer and auth view

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -1,27 +1,49 @@
-from rest_framework import serializers
-from .models import User, Profile, Emotions
+from rest_framework import serializers, validators
+from .models import User, Profile, Emotions, Task
 from django.db import transaction
+from django.utils import timezone
 
 
-class userSerializer(serializers.ModelSerializer):
-
-    def create(self, validated_data):
+class userSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    username = serializers.CharField()
+    image = serializers.URLField()
+    def save(self, **kwargs):
         with transaction.atomic():
-            user, created = User.objects.get_or_create(email=validated_data['email'], username=validated_data['username'])
+            user, created = User.objects.get_or_create(email=self.validated_data['email'], username=self.validated_data['username'])
             if not created:
                 return user
-            user.save()
-            profile = Profile(user=user, image=validated_data['image'])
-            profile.save()
-
+            Profile.objects.create(user=user, image=self.validated_data['image'])
             return user
-    image = serializers.URLField()
-    class Meta:
-        model = User
-        fields = ['email', 'username', 'image']
+    def validate(self, attrs):
+        email = attrs.get("email", None)
+        if not email:
+            raise validators.ValidationError("Email field is required")
+        username = attrs.get("username", None)
+        if not username:
+            raise validators.ValidationError("Username field is required")
+        image = attrs.get("image", None)
+        if not image:
+            raise validators.ValidationError("Image field is required")
+        return {
+            "email": email,
+            "username": username,
+            "image": image
+        }
     
 
 class emotionSerializer:
     class Meta:
         model = Emotions
         fields = ["user", "emotion"]
+    
+
+
+class taskSerializer(serializers.ModelSerializer):
+    expired = serializers.SerializerMethodField("is_expired")
+    class Meta:
+        model = Task
+        fields = ['task','user', 'reward', 'done', 'created_at', 'expire', 'user', 'expired']
+
+    def is_expired(self, task):
+        return timezone.now() > task.expire
--- backend/api/views.py
+++ backend/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView, Response
 from rest_framework.decorators import api_view
 from .google_auth import create_token
-from . import serializer as serial
+from . import serializer as serial, models
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
 
@@ -36,6 +36,30 @@ class emotionApiView(APIView):
             result = serializer.save()
             return Response(result, status=201)
 
+        except Exception as e:
+            print(e)
+            return Response({"error": "Server error"}, status=500)
+    
+class taskView(APIView):
+     def post(self, request, *args, **kwargs):
+        try:
+            data = {**request.data, "user": request.user}
+            serializer = serial.taskSerializer(data=data)
+            if not serializer.is_valid():
+                return Response(serializer.errors, status=400)
+            result = serializer.save()
+            return Response(result, status=201)
+
+        except Exception as e:
+            print(e)
+            return Response({"error": "Server error"}, status=500)
+        
+     def get(self, request, *args, **kwargs):
+        try:
+            user_id = request.user.id
+            taskes = models.User.objects.prefetch_related("task").get(id=user_id).task.all()
+            serializer = serial.taskSerializer(taskes, many=True)
+            return Response(serializer.data)
         except Exception as e:
             print(e)
             return Response({"error": "Server error"}, status=500)
