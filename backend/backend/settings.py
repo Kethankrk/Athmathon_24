0769438e7e24c34059452a5744b611dd5689cf18
Kethankrk <kethankrk@gmail.com>
2024-05-03 06:43:36 +0530

    added websocket for chat

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,7 +33,20 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
 
+
+ASGI_APPLICATION = 'backend.asgi.application'
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
+
 INSTALLED_APPS = [
+    "daphne",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -44,6 +57,7 @@ INSTALLED_APPS = [
     "api",
     "corsheaders",
     "rest_framework_simplejwt",
+    "chat",
 ]
 
 AUTH_USER_MODEL = "api.User"
